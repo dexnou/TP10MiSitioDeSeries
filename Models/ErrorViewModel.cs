6d083a586490588050a33fb4cace46073faa2209
NOAH DEMIAN ROTBARD <52096691@ort>
2023-10-18 08:53:39 -0300

    v0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP10MiSitioDeSeries.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
