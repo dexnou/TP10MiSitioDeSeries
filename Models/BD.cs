a1ef95bb86eb7cc4d1e111f71e5b4f675778550c
NOAH DEMIAN ROTBARD <52096691@ort>
2023-10-25 08:29:38 -0300

    vFinal - Sin CSS

--- Models/BD.cs
+++ Models/BD.cs
@@ -7,23 +7,33 @@ public class BD{
 private static string _connectionString = @"Server=.;DataBase=BDSeries;Trusted_Connection=True;";
     
     public static List<Series> CargarSeries(){
-        List<Series> listaSeries = new List<Series>();
-        using(SqlConnection db = new SqlConnection(_connectionString)){
+        List<Series> devolver = null;
+        using (SqlConnection db = new SqlConnection(_connectionString)){
             string sql = "SELECT * FROM Series";
-            listaSeries = db.Query<Series>(sql).ToList(); 
+            devolver = db.Query<Series>(sql).ToList();
         }
-        return listaSeries; 
+        return devolver;
     }
 
-    public static List<Actores> VerActores(int IdSerie){  
-        List<Actores> listaActores = new List<Actores>();
-        using (SqlConnection db = new SqlConnection(_connectionString))
-        {
-            string sql = "SELECT * FROM Actores WHERE IdSerie = @pIdSerie";
-            listaActores = db.Query<Actores>(sql, new { pIdSerie = IdSerie }).ToList();
+    // public static List<Actores> VerActores(int IdSerie){  
+    //     List<Actores> listaActores = new List<Actores>();
+    //     using (SqlConnection db = new SqlConnection(_connectionString))
+    //     {
+    //         string sql = "SELECT * FROM Actores WHERE IdSerie = @pIdSerie";
+    //         listaActores = db.Query<Actores>(sql, new { pIdSerie = IdSerie }).ToList();
+    //     }
+    //     return listaActores; 
+    // }
+
+    public static List<Actores> VerActores(int idSerie){
+        List<Actores> devolver = null;
+        using (SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Actores WHERE IdSerie = @IdSerie";
+            devolver = db.Query<Actores>(sql, new{IdSerie = idSerie}).ToList();
         }
-        return listaActores; 
+        return devolver;
     }
+
 
     public static List<Temporadas> VerTemporadas(int IdSerie){
         List<Temporadas> listaTemporadas = new List<Temporadas>();
